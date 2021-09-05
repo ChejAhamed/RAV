c7ab6a5086f12e4f3cc956f0ae6762efe2a81bc3
“Chej <chejahamed@gmail.com>
2021-09-05 19:47:58 +0200

    Add Login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import Header from './components/Header/Header';
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Header />
-      <h2>quiz</h2>
-      <Dashboard/>
+        <Switch>
+          <h2>quiz</h2>
+          <Route path='/' exact component={Dashboard}/>
+          <Redirect path='/dashboard' to='/'/>
+          
+        </Switch>
+    </BrowserRouter>
     </>
   );
 }
