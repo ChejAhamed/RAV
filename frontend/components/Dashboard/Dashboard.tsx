14ed589a835432095e6fc2885f8fe7499e81d71c
“Chej <chejahamed@gmail.com>
2021-09-05 16:39:37 +0200

    Add Props to question card

--- frontend/components/Dashboard/Dashboard.tsx
+++ frontend/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import QuestionCard from "../QuestionCard/QuestionCard";
+
+
 
 const Dashboard =()=>{
+    const startQuiz=async()=>{
+
+    }
+    const checkAnswer=(event:React.MouseEvent<HTMLButtonElement>)=>{
+
+    }
+    const nextQuestion = ()=>{
+
+    }
     return(
-        <div>Dashboard</div>
+        <div>Dashboard
+            <h1>React Quiz</h1>
+            <button className='start' onClick={startQuiz}> </button>
+            <p className='score'>Score:</p>
+            <p >Loading Questions...</p>
+            <QuestionCard />
+            <button className="next" onClick={nextQuestion}>Next Question</button>
+
+        </div>
     )
 }
 export default Dashboard
