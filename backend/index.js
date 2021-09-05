50a673bfc3664dd1811660528f6c129fb1d880fd
“Chej <chejahamed@gmail.com>
2021-09-05 11:19:22 +0200

    Complete config on index.js

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,25 @@ const morgan = require('morgan');
 const chalk = require('chalk');
 
 const cors = require('cors');
+
+require('./src/config/mongooseConfig');
+
+const server = express();
+const port = process.env.PORT || 5000;
+
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+
+const quizRouter = require('./src/routes/quizRoutes');
+
+server.use('/api/quiz', quizRouter);
+
+const userRouter = require('./src/routes/userRoutes');
+
+server.use('/api/user', userRouter);
+
+server.listen(
+  port,
+  () => debug(`Server is running on ${chalk.bgYellow(`http://localhost:${port}`)}`)
+);
