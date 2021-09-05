768d453440bd2d7c3397402de3c6fb14c207d902
“Chej <chejahamed@gmail.com>
2021-09-05 19:37:05 +0200

    Add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Dashboard from './components/Dashboard/Dashboard'
+import Header from './components/Header/Header';
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <h2>quiz</h2>
       <Dashboard/>
-    </div>
+    </>
   );
 }
 
