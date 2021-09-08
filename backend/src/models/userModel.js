c0cff78027af7ed5a7d3b1064c5c65d86f668d71
“Chej <chejahamed@gmail.com>
2021-09-08 19:48:50 +0200

    Config userModel

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
+const isValidPassword = require('../functions/functions');
+
 const userSchema = mongoose.Schema({
-  name: String,
-  username: String,
-  email: String,
-  score: Number,
-  quizCompleted: Number
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: { type: String, default: process.env.AVATAR },
+  totalScore: Number,
+  quizCompleted: Number,
+  rank: String
 
 });
+userSchema.methods.isValidPassword = isValidPassword;
+
 module.exports = mongoose.model('User', userSchema);
