b2a2380ca0874fef2bd38e12f7991afa55cdff55
“Chej <chejahamed@gmail.com>
2021-09-08 12:04:57 +0200

    Add score function in startQuiz

--- frontend/src/pages/StartQuiz/StartQuiz.tsx
+++ frontend/src/pages/StartQuiz/StartQuiz.tsx
@@ -11,12 +11,13 @@ const StartQuiz:React.FC =()=>{
   const quizz= useSelector((store:any)=>store.quiz)
   const dispatch = useDispatch();
   
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
   
-  console.log(numberOfQinQuiz)
-  console.log(choosenTheme)
 
 
-
+ console.log(score)
   useEffect(() => {
     dispatch(loadAllQuiz());
 
@@ -30,50 +31,94 @@ const StartQuiz:React.FC =()=>{
   //setCurrentQuiz(quizFilter)
   console.log('filtradoo',filteredQuiz);
   
-  const handleAnswerButtonClick=()=>{
-      ///check if answer is correct
+
+  
+
+
+  const handleAnswerButton=(isCorrect:any)=>{
+    console.log('hola', isCorrect)
+    console.log(score)
+    if (isCorrect) {
+			setScore(score + 1);
+		}
+    
+    const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < filteredQuiz.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
   }
+  console.log(filteredQuiz)
     return(
-    <div>
-      
+    <div className="score">
+    <div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {filteredQuiz?.length}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{filteredQuiz?.length}
+						</div>
+						<div className='question-text'>{filteredQuiz[currentQuestion]?.question}</div>
+					</div>
+					<div className='answer-section'>
+						{filteredQuiz[currentQuestion]?.answers?.map((answer:any) => (
+							<button key={answer?._id} onClick={() => handleAnswerButton(answer?.isCorrect)}>{answer?.text + `${answer?.isCorrect}`}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
 
 
 
-         {//render componet thta show score and give confety
-       }
-       {false ? (
-         
-         <EndQuiz />
-        
-       ):
 
-       <div className="Questions">
-           <div>
-             <button type="button" >Logger</button>
-             
-          </div>
+
+
+
+
+
+
+
+       {
+
+console.log(filteredQuiz[currentQuestion]?.question)}
+{console.log(filteredQuiz[currentQuestion]?.answers.text)}
+
+{
+
+       /*
+       <div className='app'>
+		
+		
+	
           <p className="score">Score:</p>
           <p>FILTERED</p>
 
           <p>Loading Questions...</p>
          
             
-          <ul> DD:{ filteredQuiz?.map((quiz: any) => 
+          <div> DD:{ filteredQuiz?.map((quiz: any) => 
             <div key={quiz?.question}>
-              <li>
-                Pregunta: {quiz?.question}
-              </li>
+              <h3>
+               {quiz?.question}
+              </h3>
               {''}
-              <ul>{quiz?.answers?.map((answer: any) => 
-                  <button type="button" key={answer?.text} onClick={() => handleAnswerButtonClick(/*answer?.isCorrect*/)}>
-                    respuesta:{answer?.text}
+              <div>{quiz?.answers?.map((answer: any) => 
+                 <button type="button" key={answer?.text} onClick={() => handleAnswerButton(answer.isCorrect)}>
+                  {answer?.text}
                   </button>
+                  
                 
                   )}
-                </ul>
+                </div>
                 </div>)}
-            </ul> 
-        </div>
+            </div> 
+        </div>*/
         }
     </div>
     )
