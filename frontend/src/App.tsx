8e0a797c9de4c92eff4e73d77cbace720e21118c
“Chej <chejahamed@gmail.com>
2021-09-09 09:13:55 +0200

    Add register page nad delete EndQuiz page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom'
 import { Provider } from 'react-redux';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import Dashboard from './pages/Dashboard/Dashboard'
+
 import Header from './components/Header/Header';
 import Profile from './pages/Profile/Profile';
 import NotFound from './pages/NotFound/NotFound';
@@ -9,20 +12,22 @@ import Footer from './components/Footer/Footer';
 import Statistics from './pages/Statistics/Statiscs';
 import StartQuiz from './pages/StartQuiz/StartQuiz';
 import configureStore from './redux/store';
-import EndQuiz from './pages/EndQuiz/EndQuiz';
+
 function App() {
   return (
     <Provider store={configureStore()}>
     <BrowserRouter>
       <Header />
         <Switch>
-          
-          <Route path='/' exact component={Dashboard}/>
-          <Redirect path='/dashboard' to='/'/>
+          <Route path='/' exact component={Login}/>
+          <Redirect path='/login' to='/'/>
+          <Route path="/register" component={Register} />
           <Route path="/profile" component={Profile} />
+
+          <Route path="/profile" component={Dashboard} />
           <Route path="/statistics" component={Statistics} />
           <Route path="/startquiz" component={StartQuiz} />
-          <Route path="/endquiz" component={EndQuiz} />
+        
           <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
